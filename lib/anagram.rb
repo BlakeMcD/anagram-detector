c98e523413fd3cb61016cbad753e9d9fb7256762
BlakeMcD <blakemcd@gmail.com>
2021-09-07 19:32:21 +1000

    assignment passed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram 
+
+    def initialize(word)
+        @word = word
+    end
+
+    attr_accessor :match
+
+    def match(array)
+        anagram_array = []
+        array.each do |word|
+            if @word.split('').sort == word.split('').sort
+                anagram_array << word
+            end    
+        end
+        anagram_array
+    end
+end
